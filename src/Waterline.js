c28a4c9c7470de02686f367f683a7d59b26c1aeb
Joshua Jabbour <code@joshuajabbour.com>
2015-03-24 00:16:38 -0700

    Add default disk adapter.

--- src/Waterline.js
+++ src/Waterline.js
@@ -2,6 +2,18 @@
 
 import Storage from 'ferry/lib/Storage';
 
-class Waterline extends Storage {};
+class Waterline extends Storage {
+
+  constructor(adapter) {
+
+    if (typeof adapter === 'undefined') {
+      adapter = require('./diskAdapter');
+    }
+
+    console.log(adapter)
+
+  }
+
+};
 
 export default Waterline;
